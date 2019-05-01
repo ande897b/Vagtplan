2a775a60879b53c62896ba80ece7ffc1195f5183
Kristoffer <Krisboettcher@live.dk>
2019-05-01 10:38:59 +0200

    i changed empRepo

--- Controller/EmployeeRepository.cs
+++ Controller/EmployeeRepository.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace Controller
 {
-    class EmployeeRepository
+    public static class EmployeeRepository
     {
+		private static List<Employee> employees = new List<Employee>();
+		public static void AddEmployee(Employee employee)
+		{
+			employees.Add(employee);
+		}
+		public  int GetEmployeeID()
+		{
+
+		}
+		public List<Employee> GetEmployees()
+		{
+			return List<Employee> employees;
+		}
+		public void RemoveEmployee()
+		{
+
+		}
     }
 }
