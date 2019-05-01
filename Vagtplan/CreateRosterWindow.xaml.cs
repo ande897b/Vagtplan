9179c589aedb61cc72b02dcde55f985e7576232f
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2019-05-01 13:12:36 +0200

    jkh

--- Vagtplan/CreateRosterWindow.xaml.cs
+++ Vagtplan/CreateRosterWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Vagtplan
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            Controller.Controller.CreateRoster(comboBoxShop.Text.ToLower(), comboBoxDuration.Text.ToLower());
+            Controller.Controller.CreateRoster(comboBoxDuration.Text.ToLower(), comboBoxShop.Text.ToLower());
             MessageBox.Show("Vagtplan med " + comboBoxDuration.Text + " i "+ comboBoxShop.Text+" oprettet. Du kan nu indsætte vagter", "Success");
             this.Close();
         }
