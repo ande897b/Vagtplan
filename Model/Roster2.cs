b2baa3b80ae141f8f8ce861a6488fa573fe56791
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2019-05-02 10:36:44 +0200

    Roster2

---
+++ Model/Roster2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Roster2
+    {
+        public int RosterID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Shop Shop { get; set; }
+
+        public Roster2(int rosterID, DateTime startDate, DateTime endDate, Shop shop)
+        {
+            RosterID = rosterID;
+            StartDate = startDate;
+            EndDate = endDate;
+            Shop = shop;
+        }
+        public Roster2(DateTime startDate, DateTime endDate, Shop shop)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Shop = shop;
+        }
+    }
+}
