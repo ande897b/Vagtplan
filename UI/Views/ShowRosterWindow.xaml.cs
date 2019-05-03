236736ee249c605ae2669851c379917c1476db4a
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2019-05-03 11:47:44 +0200

    test

--- UI/Views/ShowRosterWindow.xaml.cs
+++ UI/Views/ShowRosterWindow.xaml.cs
@@ -22,23 +22,26 @@ namespace UI.Views
         public ShowRosterWindow()
         {
             InitializeComponent();
-            int i = 1;
-            int x = 0;
-            List<Roster> rosters =  RosterRepository.GetRosters();
-            // weekday1label.Content = (rosters[1].StartDate.ToString()+ rosters[1].Shop);
-            while (rosters[i].StartDate.AddDays(x) <= rosters[i].EndDate)
-            {
-                weekday1label.Content = rosters[i].StartDate.AddDays(0+x);
-                weekday2label.Content = rosters[i].StartDate.AddDays(1 + x);
-                weekday3label.Content = rosters[i].StartDate.AddDays(2 + x);
-                weekday4label.Content = rosters[i].StartDate.AddDays(3 + x);
-                weekday5label.Content = rosters[i].StartDate.AddDays(4 + x);
-                weekday6label.Content = rosters[i].StartDate.AddDays(5 + x);
-                weekday7label.Content = rosters[i].StartDate.AddDays(6 + x);
-                x = x + 7;
+            //int i = 0;
+            //int x = 0;
+            List<Roster> rosters = RosterRepository.GetRosters();
+            //// weekday1label.Content = (rosters[1].StartDate.ToString()+ rosters[1].Shop);
 
+            //while (rosters[i].StartDate.AddDays(x) <= rosters[i].EndDate)
+            //{
+            //    weekday1label.Content = rosters[i].StartDate.AddDays(0+x);
+            //    weekday2label.Content = rosters[i].StartDate.AddDays(1 + x);
+            //    weekday3label.Content = rosters[i].StartDate.AddDays(2 + x);
+            //    weekday4label.Content = rosters[i].StartDate.AddDays(3 + x);
+            //    weekday5label.Content = rosters[i].StartDate.AddDays(4 + x);
+            //    weekday6label.Content = rosters[i].StartDate.AddDays(5 + x);
+            //    weekday7label.Content = rosters[i].StartDate.AddDays(6 + x);
+            //    x = x + 7;
 
-            }
+          
+
+            
         }
+        
     }
 }
