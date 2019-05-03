f2a34f064334fffd151108b5a22897771805305a
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2019-05-03 11:01:16 +0200

    test

--- UI/Views/ShowRosterWindow.xaml.cs
+++ UI/Views/ShowRosterWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Application;
+using Application.Repositories;
+using Domain.Models;
 
 namespace UI.Views
 {
@@ -19,6 +22,23 @@ namespace UI.Views
         public ShowRosterWindow()
         {
             InitializeComponent();
+            int i = 1;
+            int x = 0;
+            List<Roster> rosters =  RosterRepository.GetRosters();
+            // weekday1label.Content = (rosters[1].StartDate.ToString()+ rosters[1].Shop);
+            while (rosters[i].StartDate.AddDays(1) != rosters[i].EndDate)
+            {
+                weekday1label.Content = rosters[i].StartDate.AddDays(0+x);
+                weekday2label.Content = rosters[i].StartDate.AddDays(1 + x);
+                weekday3label.Content = rosters[i].StartDate.AddDays(2 + x);
+                weekday4label.Content = rosters[i].StartDate.AddDays(3 + x);
+                weekday5label.Content = rosters[i].StartDate.AddDays(4 + x);
+                weekday6label.Content = rosters[i].StartDate.AddDays(5 + x);
+                weekday7label.Content = rosters[i].StartDate.AddDays(6 + x);
+                x = x + 7;
+
+
+            }
         }
     }
 }
